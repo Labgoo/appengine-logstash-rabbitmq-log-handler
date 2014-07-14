7bc6910dd4cc134e078d9d26e7535c64fe051934
Barak Cohen <barakcoh@gmail.com>
2014-07-14 18:24:08 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from package import __version__
 
 setup(
-    name='appengine-logstash-rabbitmq-log-hanlder'
+    name='appengine-logstash-rabbitmq-log-hanlder',
     install_requires=[
         # Make this list awesome.
     ]
