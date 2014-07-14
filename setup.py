2e4c5d9e8fd79682beb884dd9b35f24896ec80b4
Barak Cohen <barakcoh@gmail.com>
2014-07-14 18:23:35 +0300

    setup.py w/ name

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from package import __version__
+
+setup(
+    name='appengine-logstash-rabbitmq-log-hanlder'
+    install_requires=[
+        # Make this list awesome.
+    ]
+)
