baa44cfdb97d022198bfb40516b0f284ef4fc3a1
Barak Cohen <barakcoh@gmail.com>
2014-07-14 18:12:09 +0300

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from package import __version__
+
+setup(
+    install_requires=[
+        # Make this list awesome.
+    ]
+)
