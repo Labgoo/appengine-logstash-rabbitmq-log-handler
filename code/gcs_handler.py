cdfa599243f4c9cb8176a387c9b7186e6c0f09e2
romankor <kournjaev@gmail.com>
2015-03-26 15:55:35 -0700

    removing logstash submodule , adding gsc upload code to our codebase

--- code/gcs_handler.py
+++ code/gcs_handler.py
@@ -1,14 +1,14 @@
-
 from logging import Handler
+from shared.services import get_google_cloud_storage_service
+from time import gmtime, strftime
 
 
 class GCSHandler(Handler):
-
     def __init__(self, gcs_bucket):
         super(GCSHandler, self).__init__()
+        self.gcs_bucket = gcs_bucket
         self.data = []
-
-        return (len(self.buffer) >= self.capacity)
+        self.storage_service = get_google_cloud_storage_service()
 
     def emit(self, record):
         self.data.append(record)
@@ -16,6 +16,16 @@ class GCSHandler(Handler):
     def flush(self):
         self.acquire()
         try:
+            current = gmtime()
+            day = strftime("%Y-%m-%d %H:%M:%S", current)
+            hour = strftime("%H", current)
+            product_gcs_patch = '%s/%s/%s' % (
+                day, hour, self.gcs_bucket
+            )
+
+            get_google_cloud_storage_service().write_file(
+                product_gcs_patch, '\n'.join(self.data), mode='w')
+
             # write to gcs
             self.data = []
         finally:
